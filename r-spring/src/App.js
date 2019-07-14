682fb8ca4011977b33d75bd3dcb2bae272c1dbca
LaanFor <niko.sky.lan@gmail.com>
2019-07-14 22:54:56 +0800

    react=spring-demo

---
+++ r-spring/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Chain from "./Demo/Chain";
+import Basic from "./Demo/Basic";
+
+function App() {
+  return (
+    <div className="App">
+      <Chain/>
+      <Basic/>
+    </div>
+  );
+}
+
+export default App;
